177f9179bf3f68fc85c0077722c99f2cde1d07aa
JoeSealy <Joe.sealy@hotmail.co.uk>
2023-01-06 00:06:13 +0000

    starting this now

--- PyTorch Learning/PyTorch Learning/PyTorch_Learning.py
+++ PyTorch Learning/PyTorch Learning/PyTorch_Learning.py
@@ -2,3 +2,18 @@
 import torch 
 
 print(torch.cuda.is_available())
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+print(device)
+
+tensor = torch.tensor([1,2,3])
+
+print(tensor, tensor.device)
+
+tensor_on_gpu = tensor.to(device)
+print(tensor_on_gpu)
+
+print(tensor_on_gpu.cpu().numpy())
+
+

aee43431e16d088e8d0f2fb0ead1cfca3b60d1cd
JoeSealy <Joe.sealy@hotmail.co.uk>
2023-01-06 15:43:06 +0000

    all done now and all correct

--- Exercises/00_PytorchExercise/00_PyExercise/00_PyExercise/_00_PyExercise.py
+++ Exercises/00_PytorchExercise/00_PyExercise/00_PyExercise/_00_PyExercise.py
@@ -2,12 +2,12 @@ import torch
 ##########################################################1
 #document reading
 
-###########################################################2
+###########################################################2 correct
 # Create random tensor
 random_tensor = torch.rand(7,7)
 print(random_tensor)
 
-###########################################################3
+###########################################################3 correct
 # Create another random tensor
 random_tensor2 = torch.rand(1,7)
 print(random_tensor2)
@@ -16,29 +16,29 @@ print(random_tensor2)
 mult_tensor = torch.matmul(random_tensor,random_tensor2.T)
 print(mult_tensor)
 
-###########################################################4
+###########################################################4 correct
 # Set manual seed
 seed = 0
 
 # Create two random tensors
 torch.manual_seed(seed)
-random_tensor3 = torch.rand(2,3)
+random_tensor3 = torch.rand(7,7)
 print(random_tensor3)
 torch.manual_seed(seed)
-random_tensor4 = torch.rand(2,3)
+random_tensor4 = torch.rand(1,7)
 print(random_tensor4)
 
 # Matrix multiply tensors
 mult_tensor2 = torch.matmul(random_tensor3,random_tensor4.T)
 print(mult_tensor2)
 
-#############################################################5
+#############################################################5 correct lol
 # Set random seed on the GPU
-print(torch.seed())
+print(torch.cuda.manual_seed(1234))
 
-#############################################################6
+#############################################################6 correct
 # Set random seed
-GPUseed = torch.seed()
+torch.manual_seed(1234)
 
 # Check for access to GPU
 print(torch.cuda.is_available())
@@ -47,32 +47,31 @@ print(torch.cuda.is_available())
 device = "cuda"
 random_tensor5 = torch.rand(2,3).to(device)
 print(random_tensor5)
-torch.manual_seed(GPUseed)
 random_tensor6 = torch.rand(2,3).to(device)
 print(random_tensor6)
 
-###############################################################7
+###############################################################7 correct
 # Perform matmul on tensor_5 and tensor_6
 mult_tensor3 = torch.matmul(random_tensor5,random_tensor6.T)
 print(mult_tensor3, mult_tensor3.shape)
 
-################################################################8
+################################################################8 correct
 # Find max
 print(torch.max(mult_tensor3))
 # Find min
 print(torch.min(mult_tensor3))
 
-#################################################################9
+#################################################################9 correct
 # Find arg max
 print(torch.argmax(mult_tensor3))
 # Find arg min
 print(torch.argmin(mult_tensor3))
 
-#################################################################10
+#################################################################10 correct
 # Set seed
 last_seed = 7
 
-# Create random tensor
+#Create random tensor
 torch.manual_seed(last_seed)
 random_tensor7 = torch.rand(1,1,1,10)
 

7b111073df5edfb0f69672ff3b9b0033f1917884
JoeSealy <Joe.sealy@hotmail.co.uk>
2023-01-09 22:12:35 +0000

    added some more

--- PyTorch Learning/PyTorch Learning/PyTorch_Learning.py
+++ PyTorch Learning/PyTorch Learning/PyTorch_Learning.py
@@ -16,3 +16,40 @@ step = 0.02
 X= torch.arange(start,end,step).unsqueeze(dim=1)
 y = weight * X + bias
 print(X[:10], y[:10] )
+
+#create a train/test split
+
+train_split = int(0.8 *len(X))
+X_train, y_train = X[:train_split], y[:train_split]
+X_test, y_test = X[train_split:], y[train_split:]
+
+#how might we visualise our data?
+#Visualise, visualise, visualise
+
+def plot_predictions(train_data=X_train, 
+                     train_labels=y_train, 
+                     test_data=X_test,
+                     test_labels =y_test,
+                     predictions=None):
+    """
+    plots training data, test data and compares predictions.
+    """
+    plt.figure(figsize = (10,7))
+
+    #Plot training data in blue
+
+    plt.scatter(train_data, train_labels, c="b", s=4, label="Training data")
+
+    plt.scatter(test_data, test_labels, c="g", s=4, label="Test data")
+
+    #are there predictions
+    if predictions is not None:
+        plt.scatter(test_data,predictions, c="r", s=4, label="Predictions")
+
+    plt.legend(prop={"size":14})
+
+    plt.show()
+
+print(plot_predictions())
+
+class LinearRegressionModel(nn.Module):

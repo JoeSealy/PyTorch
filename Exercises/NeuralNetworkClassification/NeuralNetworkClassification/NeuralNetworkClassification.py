04436f16070696b469344eb02a0bdd58b16bf54a
JoeSealy <Joe.sealy@hotmail.co.uk>
2023-01-16 23:50:27 +0000

    bit more

--- Exercises/NeuralNetworkClassification/NeuralNetworkClassification/NeuralNetworkClassification.py
+++ Exercises/NeuralNetworkClassification/NeuralNetworkClassification/NeuralNetworkClassification.py
@@ -1,1 +1,14 @@
+#2 Neural Network Classification
 
+import sklearn
+from sklearn.datasets import make_circles
+
+n_samples = 1000
+
+X, y = make_circles(n_samples,
+                    noise=0.03,
+                    random_state=42)
+
+print(len(X), len(y))
+print(f"First 5 samples of X:\n{X[:5]}")
+print(f"First 5 samples of y:\n{y[:5]}")

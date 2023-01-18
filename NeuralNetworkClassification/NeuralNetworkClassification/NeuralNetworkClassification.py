62d9f48ff4a45ad8cc943403c6139aa24a802a06
JoeSealy <Joe.sealy@hotmail.co.uk>
2023-01-18 01:04:31 +0000

    added the model and training loop and the graph shows not much gonna add more layers

--- NeuralNetworkClassification/NeuralNetworkClassification/NeuralNetworkClassification.py
+++ NeuralNetworkClassification/NeuralNetworkClassification/NeuralNetworkClassification.py
@@ -4,6 +4,7 @@ import sklearn
 from sklearn.datasets import make_circles
 from sklearn.model_selection import train_test_split
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import torch
 from torch import nn
@@ -61,6 +62,7 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 print(device)
 print(X_test)
 print(X_train.shape)
+# if not doing much with it, this is pointless, nn.sequential is better
 class CircleModelV0(nn.Module):
     def __init__(self):
         super().__init__()
@@ -121,16 +123,75 @@ print(y_preds.squeeze())
 #training and testing loop
 torch.cuda.manual_seed(42)
 torch.manual_seed(42)
-epochs = 1
+epochs = 1000
 
 X_train, y_train = X_train.to(device), y_train.to(device)
-
+X_test, y_test = X_test.to(device), y_test.to(device)
 
 for epoch in range(epochs):
-
+    model_0.train()
+    y_logits = model_0(X_train).squeeze()
+    y_pred = torch.round(torch.sigmoid(y_logits))
     
+    loss = loss_fn(y_logits,
+                   y_train)
+    acc = accuracy_fn(y_true=y_train,
+                      y_pred=y_pred)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
 
+    model_0.eval()
+    with torch.inference_mode():
+        test_logits = model_0(X_test).squeeze()
+        test_pred = torch.round(torch.sigmoid(test_logits))
+        test_loss = loss_fn(test_logits,
+                            y_test)
+        test_acc = accuracy_fn(y_true=y_test,
+                               y_pred=test_pred)
+    if epoch % 10 == 0:
+        print(f"Epoch: {epoch} | Loss: {loss:.5f}, Acc: {acc:.2f}% | Test Loss: {test_loss:.5f}, Test acc: {test_acc:.2f}% ")
+    
+def plot_decision_boundary(model: torch.nn.Module, X: torch.Tensor, y: torch.Tensor):
+    model.to("cpu")
+    X, y = X.to("cpu"), y.to("cpu")
 
+    x_min, x_max = X[:, 0].min() - 0.1, X[:, 0].max() + 0.1
+    y_min, y_max = X[:, 1].min() - 0.1, X[:, 1].max() + 0.1
+    xx, yy = np.meshgrid(np.linspace(x_min, x_max, 101), np.linspace(y_min, y_max, 101))
+
+    # Make features
+    X_to_pred_on = torch.from_numpy(np.column_stack((xx.ravel(), yy.ravel()))).float()
+
+    # Make predictions
+    model.eval()
+    with torch.inference_mode():
+        y_logits = model(X_to_pred_on)
+
+    # Test for multi-class or binary and adjust logits to prediction labels
+    if len(torch.unique(y)) > 2:
+        y_pred = torch.softmax(y_logits, dim=1).argmax(dim=1)  # mutli-class
+    else:
+        y_pred = torch.round(torch.sigmoid(y_logits))  # binary
+
+    # Reshape preds and plot
+    y_pred = y_pred.reshape(xx.shape).detach().numpy()
+    plt.contourf(xx, yy, y_pred, cmap=plt.cm.RdYlBu, alpha=0.7)
+    plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.RdYlBu)
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+
+plt.figure(figsize=(12, 6))
+
+plt.subplot(1, 2, 1)
+plt.title("Train")
+plot_decision_boundary(model_0, X_train, y_train)
+plt.show()
+
+plt.subplot(1, 2, 2)
+plt.title("Test")
+plot_decision_boundary(model_0, X_test, y_test)
+plt.show()
 
 
 

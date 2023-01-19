d54ebcfd13d5845c3235d54bca47e19ea5066bf4
JoeSealy <Joe.sealy@hotmail.co.uk>
2023-01-19 18:24:19 +0000

    finished NNclassicifcation

--- NeuralNetworkClassification/NeuralNetworkClassification/NeuralNetworkClassification.py
+++ NeuralNetworkClassification/NeuralNetworkClassification/NeuralNetworkClassification.py
@@ -1,7 +1,7 @@
 #2 Neural Network Classification
 
 import sklearn
-from sklearn.datasets import make_circles
+from sklearn.datasets import make_circles, make_blobs
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
@@ -24,10 +24,10 @@ circles = pd.DataFrame({"X1": X[:, 0],
 #print(circles.head(10))
 
 #visualise
-#plt.scatter(x=X[:, 0],
-#            y=X[:, 1],
- #           c=y,
- #           cmap=plt.cm.RdYlBu);
+plt.scatter(x=X[:, 0],
+            y=X[:, 1],
+            c=y,
+            cmap=plt.cm.RdYlBu);
 #plt.show()
 
 #check input and output shapes
@@ -151,7 +151,7 @@ for epoch in range(epochs):
                                y_pred=test_pred)
     if epoch % 10 == 0:
         print(f"Epoch: {epoch} | Loss: {loss:.5f}, Acc: {acc:.2f}% | Test Loss: {test_loss:.5f}, Test acc: {test_acc:.2f}% ")
-   
+"""   
 def plot_decision_boundary(model: torch.nn.Module, X: torch.Tensor, y: torch.Tensor):
     model.to("cpu")
     X, y = X.to("cpu"), y.to("cpu")
@@ -181,7 +181,7 @@ def plot_decision_boundary(model: torch.nn.Module, X: torch.Tensor, y: torch.Ten
     plt.xlim(xx.min(), xx.max())
     plt.ylim(yy.min(), yy.max())
 
-
+"""
 plt.figure(figsize=(12, 6))
 
 plt.subplot(1, 2, 1)
@@ -249,7 +249,7 @@ plt.subplot(1, 2, 2)
 plt.title("Test")
 plot_decision_boundary(model_1, X_test, y_test)
 plt.show()
-"""
+
 #-------------------------------------------------------------------------
 device = "cuda" if torch.cuda.is_available() else "cpu"
 weight = 0.7
@@ -272,9 +272,9 @@ print(len(X_train_regression), len(X_test_regression), len(y_train_regression), 
 def plot_predictions(
     train_data, train_labels, test_data, test_labels, predictions=None
 ):
-    """
-  Plots linear training data and test data and compares predictions.
-  """
+ 
+ # Plots linear training data and test data and compares predictions.
+
     plt.figure(figsize=(10, 7))
 
     # Plot training data in blue
@@ -297,6 +297,256 @@ plot_predictions(train_data = X_train_regression,
                 test_labels = y_test_regression)
 
 model_2 = nn.Sequential(
-    nn.Linear(in_features =1, out_feature=10),
-    nn.Linear(in_features =1, out_feature=10),
-    nn.Linear(in_features =1, out_feature=10)).to(device)
+    nn.Linear(in_features =1, out_features=10),
+    nn.Linear(in_features =10, out_features=10),
+    nn.Linear(in_features =10, out_features=1)).to(device)
+
+loss_fn = nn.L1Loss()
+optimizer = torch.optim.SGD(model_2.parameters(), lr = 0.01)
+
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+
+epochs = 1000
+
+X_train_regression, y_train_regression = X_train_regression.to(device), y_train_regression.to(device)
+X_test_regression, y_test_regression = X_test_regression.to(device), y_test_regression.to(device)
+
+
+for epoch in range(epochs):
+    y_pred = model_2(X_train_regression)
+    loss = loss_fn(y_pred, y_train_regression)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    model_2.eval()
+    with torch.inference_mode():
+        test_pred = model_2(X_test_regression)
+        test_loss = loss_fn(test_pred, y_test_regression)
+
+    if epoch % 100 == 0:
+        print(f"Epoch: {epoch} | Test Loss: {test_loss:.5f} | Loss: {loss:.5f}")
+
+model_2.eval()
+with torch.inference_mode():
+    y_preds= model_2(X_test_regression)
+
+plot_predictions(train_data=X_train_regression.cpu(),
+                 train_labels=y_train_regression.cpu(),
+                 test_data=X_test_regression.cpu(),
+                 test_labels = y_test_regression.cpu(),
+                 predictions = y_preds.cpu())
+------------------------------------------------------------------------------------------------------------
+device = "cuda" if torch.cuda.is_available() else "cpu"
+n_samples = 1000
+
+X, y = make_circles(n_samples,
+                    noise=0.03,
+                    random_state=42)
+
+plt.scatter(x=X[:, 0],
+            y=X[:, 1],
+            c=y,
+            cmap=plt.cm.RdYlBu);
+#plt.show()
+
+X = torch.from_numpy(X).type(torch.float)
+y = torch.from_numpy(y).type(torch.float)
+
+X_train,X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+class CircleModelV2(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer_1 = nn.Linear(in_features = 2, out_features = 10) 
+        self.layer_2 = nn.Linear(in_features = 10, out_features = 10) 
+        self.layer_3 = nn.Linear(in_features = 10, out_features = 1) 
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        return self.layer_3(self.relu(self.layer_2(self.relu(self.layer_1(x)))))
+
+model_3 = CircleModelV2().to(device)
+
+loss_fn = nn.BCEWithLogitsLoss()
+optimizer = torch.optim.SGD(params=model_3.parameters(),lr=0.1)
+def accuracy_fn(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item()
+    acc = (correct/len(y_pred)) * 100
+    return acc
+
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+
+X_train, y_train = X_train.to(device), y_train.to(device)
+X_test, y_test = X_test.to(device), y_test.to(device)
+
+epochs = 1000
+
+for epoch in range(epochs):
+    model_3.train()
+    y_logits = model_3(X_train).squeeze()
+    y_pred = torch.round(torch.sigmoid(y_logits))
+
+    loss = loss_fn(y_logits, y_train)
+    acc = accuracy_fn(y_true = y_train,
+                      y_pred=y_pred)
+
+    optimizer.zero_grad()
+
+    loss.backward()
+
+    optimizer.step()
+
+    model_3.eval()
+    with torch.inference_mode():
+        test_logits = model_3(X_test).squeeze()
+        test_pred = torch.round(torch.sigmoid(test_logits))
+
+        test_loss = loss_fn(test_logits, y_test)
+        test_acc = accuracy_fn(y_true = y_test,
+                               y_pred = test_pred)
+
+    if epoch % 100 == 0:
+       print(f"Epoch: {epoch} | Loss: {loss:.5f}, Acc: {acc:.2f}% | Test Loss: {test_loss:.4f}, Test acc: {test_acc:.2f}% ")
+    
+model_3.eval()
+with torch.inference_mode():
+    y_preds = torch.round(torch.sigmoid(model_3(X_test))).squeeze()
+y_preds[:10], y_test[:10]
+
+plt.figure(figsize=(12, 6))
+
+plt.subplot(1, 2, 1)
+plt.title("Train")
+plot_decision_boundary(model_3, X_train, y_train)
+plt.show()
+
+plt.subplot(1, 2, 2)
+plt.title("Test")
+plot_decision_boundary(model_3, X_test, y_test)
+plt.show()
+
+
+
+
+
+A = torch.arange(-10, 10, 1, dtype=torch.float32)
+plt.plot(A);
+def relu(x):
+  return torch.maximum(torch.tensor(0), x)
+relu(A)
+plt.plot(relu(A));
+
+def sigmoid(x):
+  return 1 / (1 + torch.exp(-x))
+
+sigmoid(A)
+plt.plot(sigmoid(A));
+"""
+#------------------------------------------------------------------------------------------------------------------
+
+NUM_CLASSES = 4
+NUM_FEATURES = 2
+RANDOM_SEED = 50
+
+X_blob, y_blob = make_blobs(n_samples = 1000,
+                            n_features = NUM_FEATURES,
+                            centers = NUM_CLASSES,
+                            cluster_std = 1.5,
+                            random_state=RANDOM_SEED)
+
+X_blob = torch.from_numpy(X_blob).type(torch.float)
+y_blob = torch.from_numpy(y_blob).type(torch.LongTensor)
+
+X_blob_train, X_blob_test, y_blob_train, y_blob_test = train_test_split(X_blob, y_blob, test_size=0.2, random_state = RANDOM_SEED)
+
+plt.figure(figsize = (10, 7))
+plt.scatter(X_blob[:, 0], X_blob[:, 1], c=y_blob, cmap= plt.cm.RdYlBu)
+plt.show()
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+class BlobModel(nn.Module):
+    def __init__(self, input_features, output_features, hidden_units=8):
+
+        super().__init__()
+        self.linear_layer_stack = nn.Sequential(
+            nn.Linear(in_features = input_features, out_features=hidden_units),
+            nn.ReLU(),
+            nn.Linear(in_features = hidden_units, out_features=hidden_units),
+            nn.ReLU(),
+            nn.Linear(in_features = hidden_units, out_features=output_features)
+        )
+    def forward(self, x):
+        return self.linear_layer_stack(x)
+
+model_4 = BlobModel(input_features=2, output_features=4,hidden_units=8).to(device)
+print(model_4)
+
+loss_fn=nn.CrossEntropyLoss()
+
+optimizer = torch.optim.SGD(params=model_4.parameters(), lr=0.1)
+
+def accuracy_fn(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item()
+    acc = (correct/len(y_pred)) * 100
+    return acc
+
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+epochs = 100
+
+X_blob_train, y_blob_train = X_blob_train.to(device), y_blob_train.to(device)
+X_blob_test, y_blob_test = X_blob_test.to(device), y_blob_test.to(device)
+
+for epoch in range(epochs):
+
+    model_4.train()
+
+
+    y_logits = model_4(X_blob_train)
+    y_pred = torch.softmax(y_logits, dim=1).argmax(dim=1)
+
+    loss = loss_fn(y_logits, y_blob_train) 
+
+    acc = accuracy_fn(y_true=y_blob_train,
+                      y_pred=y_pred)
+
+    optimizer.zero_grad()
+
+    loss.backward()
+
+    optimizer.step()
+
+    model_4.eval()
+
+    with torch.inference_mode():
+      test_logits = model_4(X_blob_test)
+      test_pred = torch.softmax(test_logits, dim=1).argmax(dim=1)
+      test_loss = loss_fn(test_logits, y_blob_test)
+      test_acc = accuracy_fn(y_true=y_blob_test,
+                             y_pred=test_pred)
+
+    if epoch % 10 == 0:
+        print(f"Epoch: {epoch} | Loss: {loss:.5f}, Acc: {acc:.2f}% | Test Loss: {test_loss:.5f}, Test Acc: {test_acc:.2f}%")
+
+model_4.eval()
+with torch.inference_mode():
+    y_logits = model_4(X_blob_test)
+y_logits[:10]
+
+y_preds_probs = torch.softmax(y_logits, dim = 1)
+
+plt.figure(figsize=(12, 6))
+
+plt.subplot(1, 2, 1)
+plt.title("Train")
+plot_decision_boundary(model_4, X_blob_train, y_blob_train)
+plt.show()
+
+plt.subplot(1, 2, 2)
+plt.title("Test")
+plot_decision_boundary(model_4, X_blob_test, y_blob_test)
+plt.show()

5e021333ef08ff5a9967e8214f663b36a4102c00
JoeSealy <Joe.sealy@hotmail.co.uk>
2023-01-23 01:07:45 +0000

    finished the exercise now onto the extra ciriculum

--- Exercises/02_PyTorchExerciseNNClass/02_PyTorchExerciseNeuralNetworkClassification/02_PyTorchExerciseNeuralNetworkClassification/_02_PyTorchExerciseNeuralNetworkClassification.py
+++ Exercises/02_PyTorchExerciseNNClass/02_PyTorchExerciseNeuralNetworkClassification/02_PyTorchExerciseNeuralNetworkClassification/_02_PyTorchExerciseNeuralNetworkClassification.py
@@ -5,6 +5,7 @@ from sklearn.datasets import make_moons
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from torch import nn
 """
 # Import torch
 import torch
@@ -155,7 +156,7 @@ for epoch in range(epochs):
 # Import torch
 
 # Plot the model predictions
-
+"""
 import numpy as np
 
 # TK - this could go in the helper_functions.py and be explained there
@@ -193,7 +194,7 @@ def plot_decision_boundary(model, X, y):
     plt.xlim(xx.min(), xx.max())
     plt.ylim(yy.min(), yy.max())
 
-
+"""
     # Plot decision boundaries for training and test sets
 plt.figure(figsize=(12, 6))
 plt.subplot(1, 2, 1)
@@ -207,7 +208,8 @@ plt.show()
 """     
 #-----------------------------------------------------------------------------------------
 # Create a straight line tensor
-
+RANDOM_SEED = 42
+device = "cuda" if torch.cuda.is_available() else "cpu"
 tensor = torch.arange(-100, 100, 1)
 plt.plot(tensor)
 plt.show()
@@ -221,4 +223,101 @@ def tanh(z):
 	return (torch.exp(z) - torch.exp(-z)) / (torch.exp(z) + torch.exp(-z))
 
 plt.plot(tanh(tensor))
+plt.show()
+
+N = 100 # number of points per class
+D = 2 # dimensionality
+K = 3 # number of classes
+X = np.zeros((N*K,D)) # data matrix (each row = single example)
+y = np.zeros(N*K, dtype='uint8') # class labels
+for j in range(K):
+  ix = range(N*j,N*(j+1))
+  r = np.linspace(0.0,1,N) # radius
+  t = np.linspace(j*4,(j+1)*4,N) + np.random.randn(N)*0.2 # theta
+  X[ix] = np.c_[r*np.sin(t), r*np.cos(t)]
+  y[ix] = j
+# lets visualize the data:
+plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.Spectral)
+plt.show()
+
+
+
+# Turn data into tensors
+X = torch.from_numpy(X).type(torch.float) # features as float32
+y = torch.from_numpy(y).type(torch.LongTensor) # labels need to be of type long
+X,y= X.to(device), y.to(device)
+# Create train and test splits
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=RANDOM_SEED)
+len(X_train), len(X_test), len(y_train), len(y_test)
+
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+class SpiralModel(nn.Module): 
+  def __init__(self):
+    super().__init__()
+    self.linear1 = nn.Linear(in_features=2, out_features=10)
+    self.linear2 = nn.Linear(in_features=10, out_features=10)
+    self.linear3 = nn.Linear(in_features=10, out_features=3)
+    self.relu = nn.ReLU()
+
+  def forward(self, x):
+    return self.linear3(self.relu(self.linear2(self.relu(self.linear1(x)))))
+
+model_1 = SpiralModel().to(device)
+
+# Setup loss function and optimizer
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model_1.parameters(),
+                             lr=0.02)  
+
+# Build a training loop for the model
+epochs = 1000
+
+# Loop over data
+for epoch in range(epochs):
+  ## Training
+  model_1.train()
+  # 1. forward pass
+  y_logits = model_1(X_train)
+  y_pred = torch.softmax(y_logits, dim=1).argmax(dim=1)
+
+  # 2. calculate the loss
+  loss = loss_fn(y_logits, y_train)
+ # acc = acc_fn(y_pred, y_train)
+  
+  # 3. optimizer zero grad
+  optimizer.zero_grad()
+
+  # 4. loss backwards
+  loss.backward()
+
+  # 5. optimizer step step step
+  optimizer.step()
+
+  ## Testing
+  model_1.eval()
+  with torch.inference_mode():
+    # 1. Forward pass
+    test_logits = model_1(X_test)
+    test_pred = torch.softmax(test_logits, dim=1).argmax(dim=1)
+    # 2. Caculate loss and acc
+    test_loss = loss_fn(test_logits, y_test)
+    #test_acc = acc_fn(test_pred, y_test)
+
+  # Print out what's happening
+  if epoch % 100 == 0:
+    print(f"Epoch: {epoch} | Loss: {loss:.2f} | Test loss: {test_loss:.2f}") 
+
+
+    # Plot decision boundaries for training and test sets
+plt.figure(figsize=(12, 6))
+plt.subplot(1, 2, 1)
+plt.title("Train")
+plot_decision_boundary(model_1, X_train, y_train)
+plt.show()
+plt.subplot(1, 2, 2)
+plt.title("Test")
+plot_decision_boundary(model_1, X_test, y_test)
 plt.show()

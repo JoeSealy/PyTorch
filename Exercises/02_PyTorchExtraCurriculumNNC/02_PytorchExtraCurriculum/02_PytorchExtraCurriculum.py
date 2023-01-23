a9cedeac5f0b494fa021abb2b0df11c77ff4ed11
JoeSealy <Joe.sealy@hotmail.co.uk>
2023-01-23 01:39:53 +0000

    finished some extra cirriculum things and changed some folders and renamed them

---
+++ Exercises/02_PyTorchExtraCurriculumNNC/02_PytorchExtraCurriculum/02_PytorchExtraCurriculum.py
@@ -0,0 +1,27 @@
+"""
+1. figuring out punch mechanics in boxing, such as faints, 
+blocks and diferent styles of punches
+
+2.figuring out a persons details through a camera such as; 
+figuring out height
+Race
+clothing
+and using this to figure out weight
+and spoting them in a crowd
+
+3. figuring out fake and genuine reviews on products
+
+"""
+
+"""
+Momentum 
+this is used to accelerate gradient vectors in the right direction
+thus leading to faster converging
+
+This algorithm is used to accelerate the gradient descent 
+algorithm by taking into consideration the 'exponentially weighted 
+average' of the gradients
+
+
+
+"""

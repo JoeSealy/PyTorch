b8619f2bcf45bad08482b2d827d87efe5059e305
JoeSealy <Joe.sealy@hotmail.co.uk>
2023-02-05 22:54:47 +0000

    fixed eval

--- ComputerVision/ComputerVision/ComputerVision.py
+++ ComputerVision/ComputerVision/ComputerVision.py
@@ -376,13 +376,49 @@ total_train_time_model_1 = print_train_time(start=trainTimeStartOnGpu,
                                             end=train_time_end_on_gpu,
                                             device=device)
 
+# Move values to device
+torch.manual_seed(42)
+def eval_model(model: torch.nn.Module, 
+               data_loader: torch.utils.data.DataLoader, 
+               loss_fn: torch.nn.Module, 
+               accuracy_fn, 
+               device: torch.device = device):
+    """Evaluates a given model on a given dataset.
 
-model_1_results = eval_model(model=model_1,
-                  data_loader=test_dataloader,
-                    loss_fn=loss_fn,
-                    accuracy_fn=accuracy_fn)
+    Args:
+        model (torch.nn.Module): A PyTorch model capable of making predictions on data_loader.
+        data_loader (torch.utils.data.DataLoader): The target dataset to predict on.
+        loss_fn (torch.nn.Module): The loss function of model.
+        accuracy_fn: An accuracy function to compare the models predictions to the truth labels.
+        device (str, optional): Target device to compute on. Defaults to device.
+
+    Returns:
+        (dict): Results of model making predictions on data_loader.
+    """
+    loss, acc = 0, 0
+    model.eval()
+    with torch.inference_mode():
+        for X, y in data_loader:
+            # Send data to the target device
+            X, y = X.to(device), y.to(device)
+            y_pred = model(X)
+            loss += loss_fn(y_pred, y)
+            acc += accuracy_fn(y_true=y, y_pred=y_pred.argmax(dim=1))
+        
+        # Scale loss and acc
+        loss /= len(data_loader)
+        acc /= len(data_loader)
+    return {"model_name": model.__class__.__name__, # only works when model was created with a class
+            "model_loss": loss.item(),
+            "model_acc": acc}
+
+# Calculate model 1 results with device-agnostic code 
+model_1_results = eval_model(model=model_1, data_loader=test_dataloader,
+    loss_fn=loss_fn, accuracy_fn=accuracy_fn,
+    device=device
+)
+
 print(model_1_results)
-
 
 
 
